f40f1a675d841ea534927e833868effe80ee2a70
David37 <davidkondic@protonmail.com>
2020-10-15 16:30:33 +0200

    updated signup.api.php

--- include/configs/signup.api.php
+++ include/configs/signup.api.php
@@ -9,9 +9,13 @@ if(isset($_POST['signup-submit'])){
     // set up variable names
     $firstName= $_POST['f-name'];
     $lastName= $_POST['l-name'];
+    $contactNum= $_POST['c-num'];
     $email= $_POST['email'];
+    $username= $_POST['uid'];
     $password= $_POST['pwd'];
     $passwordRepeat= $_POST['pwd-repeat'];
+    $collectionPoint="";
+    $userType="client";
     // user data validation
     if(empty($email) || empty($firstName) || empty($lastName) || empty($password) || empty($passwordRepeat)){ // checks if username is empty
         header("Location: ". $_SESSION['previous_location'] . "?error");
@@ -35,10 +39,11 @@ if(isset($_POST['signup-submit'])){
     }
     else{
         //checks if the username is already inside the database
-        $query="SELECT email FROM user WHERE email=?";
+        $query="SELECT email FROM client WHERE email = ?";
         $stmt=mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt,$query)){
             //Sql error within query
+            echo "invalid query";
         }else{ 
             // runs information inside the database
             mysqli_stmt_bind_param($stmt,"s",$email);
@@ -47,18 +52,29 @@ if(isset($_POST['signup-submit'])){
             $resultCheck=mysqli_stmt_num_rows($stmt); // gets the amount of results from the database
             if($resultCheck>0){
                 // error user email is already in the database
+                echo "This email already exists";
             }
             else{ // users data is valid and can be inserted into the database
-                $query="INSERT INTO users (email,first_name,pwd) VALUES(?,?,?)";
+                $clientQuery="INSERT INTO client (firstName,lastName, contactNumber, email, initialCollectionPoint) VALUES(?,?,?,?,?)";
+                $passwordsQuery= "INSERT INTO passwords (email, userName, password, usertype) VALUES(?,?,?,?);";
                 $stmt= mysqli_stmt_init($conn);
-                if(!mysqli_stmt_prepare($stmt,$query)){
-                    // query failed, user data cannot be inserted into the database
-                }else{
-                    $hashedPwd= password_hash($password,PASSWORD_DEFAULT); // encrypts the users password
-                    mysqli_stmt_bind_param($stmt,"sss",$email,$firstName,$hashedPwd);
+                if(!mysqli_stmt_prepare($stmt,$clientQuery)){
+                        // query failed, user data cannot be inserted into the database
+                        echo "data could not be entered into the database";
+                } else{
+                    mysqli_stmt_bind_param($stmt,"sssss",$firstName,$lastName,$contactNum,$email,$collectionPoint);
                     mysqli_stmt_execute($stmt);
-                    header("Location: index.php");
-                    exit();
+                    if(!mysqli_stmt_prepare($stmt,$passwordsQuery)){
+                        // query failed, user data cannot be inserted into the database
+                        echo "data could not be entered into the passwords database";
+                    } else{
+                        $hashedPwd= password_hash($password,PASSWORD_DEFAULT); // encrypts the users password using BCRYPT
+                        mysqli_stmt_bind_param($stmt,"ssss",$email,$username,$hashedPwd,$userType);
+                        mysqli_stmt_execute($stmt);
+                        header("Location: ../../index.php");
+                        exit();
+                    }
+                    
                 }
             }
 
